77e11f13bf5f6746b722362525079992d9a784c9
Valery <valeri0nika.viva@gmail.com>
2018-04-01 13:21:59 +0300

    Prepare data for visualization

--- src/screens/UkraineAndOtherScreen.js
+++ src/screens/UkraineAndOtherScreen.js
@@ -3,16 +3,57 @@ import {
   View, Text, TouchableOpacity, StyleSheet, TextInput
 } from 'react-native';
 import Autocomplete from 'react-native-autocomplete-input';
+import AutocompleteComponent from '../components/AutocompleteComponent'
 
 class UkraineAndOtherScreen extends Component {
+
+  requestDataForChart(obj) {
+    // start show loader
+    // TODO: start scrolling
+
+    const chartUrl = 'http://localhost:3000/rest/graph3/data?gnum=202&year=2014&cmdcode=73'
+    // console.log(chartUrl + obj.code);
+    // fetch(chartUrl + obj.code)
+    fetch(chartUrl)
+      .then(function(response) {
+        // parse data to json format
+        return response.json();
+      })
+      .then(function(myJsonArr) {
+        // prepare data for visualization
+        console.log("data response");
+        console.log(myJsonArr);
+
+        const reduceFunction = (accumulator, currentValue) => {
+          const intValue = parseInt(currentValue.contract_volume);
+          if (accumulator[currentValue.country_to]) {
+            accumulator[currentValue.country_to] += intValue;
+          } else {
+            accumulator[currentValue.country_to] = intValue;
+          }
+          return accumulator;
+        };
+
+        const dictValue = myJsonArr.data.filter(item => item.country_from === 'Україна')
+          // TODO: need to tranform to common currency
+          .reduce(reduceFunction, {})
+
+        console.log(dictValue);
+      }.bind(this));
+  }
 
   render() {
     return (
       <View>
         <Text>Select product</Text>
+        <AutocompleteComponent
+          callback={obj => { this.requestDataForChart(obj) } }
+        />
       </View>
     )
   }
 }
+
+
 
 export default UkraineAndOtherScreen;
